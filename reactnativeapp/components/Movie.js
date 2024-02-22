0e217289bcf05257fc797fa9a3302a8f981a0de0
JoniMop <jonatangurari@gmail.com>
2024-02-22 12:29:45 -0600

    Adding style, logic to search movie, button

--- reactnativeapp/components/Movie.js
+++ reactnativeapp/components/Movie.js
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
 
 const Movie = () => {
+    const[searchText, setSearchText] = useState("")
+    const onSearchMovie = (textInput) => {
+        setSearchText(textInput)
+    }
+    const findMovies = () => {
+        console.log(searchText)
+    }
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: 'center' }}>
       <Text>Movie</Text>
+      <TextInput 
+        value={searchText}
+        onChangeText={onSearchMovie}
+        placeholder="Enter movie name"
+      />
+      <Button title='Find movies' onPress={findMovies} />
     </View>
   )
 }
